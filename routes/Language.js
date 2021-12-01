7bff2d6526c635074cbc06355a923e93b20112dc
aishuupp99 <89593681+aishuupp99@users.noreply.github.com>
2021-11-30 19:56:50 -0600

    modified

--- routes/Language.js
+++ routes/Language.js
@@ -4,6 +4,17 @@ var router = express.Router();
 /* GET languages */
 router.get('/', language_controlers.language_view_all_Page );
 
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+function secured(req, res, next) {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+} 
+
 // GET request for one language. 
 router.get('/language/:id', language_controlers.language_detail); 
 
@@ -14,7 +25,7 @@ router.get('/detail', language_controlers.language_view_one_Page);
 router.get('/create', language_controlers.language_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', language_controlers.language_update_Page); 
+router.get('/update',secured, language_controlers.language_update_Page); 
 
 /* GET create language page */
 router.get('/delete', language_controlers.language_delete_Page);
